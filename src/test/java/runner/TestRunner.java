e7290863d34b924cf5361a16850ac730cbb98433
covie <covie@planittesting.com>
2020-11-18 18:49:40 +0000

    Work

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -10,8 +10,7 @@ import cucumber.api.junit.Cucumber;
 		
 		features = {"src/test/resources"},  //Path of feature file
 		glue =     {"stepsDef"},            //path of step definition
-		//format =   {"pretty", "json:target/cucumber.json","html:target/site/cucumber-pretty"},  //generate format for the test
-				 //test
+
 format ={"pretty","json:target/cucumber-reports/Cucumber.json","junit:target/cucumber-reports/Cucumber.xml","html:target/cucumber-reports" },
 		monochrome = true,  //display the console in a proper readable format
 		dryRun  = false,  //check the mapping is proper between the feature file and step def file
