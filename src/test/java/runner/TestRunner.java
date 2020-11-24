6642ff3f79c10452327dd515645c9fa9899234ae
covie <covie@planittesting.com>
2020-11-24 02:57:59 +0000

    Refactor Login and contact test

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -10,8 +10,8 @@ import cucumber.api.junit.Cucumber;
 		
 		features = {"src/test/resources"},  //Path of feature file
 		glue =     {"stepsDef"},            //path of step definition
-		format ={"pretty","html:target/cucumber-html-report","json:target/cucumber.json"},
-		//plugin ={"pretty","html:target/cucumber-html-report","json:target/cucumber.json"},
+		format ={"pretty","html:target/cucumber-html-report",  "json:target/cucumber.json"},
+
 		monochrome = true,  //display the console in a proper readable format
 		dryRun  = false//check the mapping is proper between the feature file and step def file
 		//tags = {"@login"}  //select test to run
